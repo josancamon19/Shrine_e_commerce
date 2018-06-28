7dd8f7448a6de6f42eee8b9d594f60ada882dae4
josancamon19 <joan.santiago.cabezas@gmail.com>
2018-06-28 09:48:59 -0500

    mdc-102 completed

---
+++ lib/home.dart
@@ -0,0 +1,93 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/data.dart';
+import 'model/product.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: Text('SHRINE'),
+          leading: new IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => debugPrint("Menu button")),
+          actions: <Widget>[
+            new IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () => debugPrint("search touched")),
+            new IconButton(
+                icon: Icon(Icons.tune),
+                onPressed: () => debugPrint("tune touched")),
+          ],
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildCards(context),
+        ));
+  }
+
+  List<Card> _buildCards(BuildContext context) {
+    List<Product> products = getProducts(Category.all);
+    if (products != null && products.isEmpty) {
+      return const <Card>[];
+    }
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    return products.map((product) {
+      return Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            new Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }).toList();
+  }
+}
